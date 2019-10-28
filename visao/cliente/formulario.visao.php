1d86fdf326c40ea9964e5c76452412470180eafb
1831399 <1831399@LABCMAQ08.itp.local>
2019-10-28 10:45:08 -0300

    web_loja

--- visao/cliente/formulario.visao.php
+++ visao/cliente/formulario.visao.php
@@ -10,8 +10,13 @@ if (ehPost()) {
 <link rel="stylesheet" href="./publico/css/lispro.css">
 
 <form action="" method="POST">
+    <p id="cen">Nome</p><input id="cen" type="text" name="nome" value="<?=@$cliente['nome']?>"><br><br>
     <p id="cen">E-mail</p><input id="cen" type="text" name="email" value="<?=@$cliente['email']?>"><br><br>
     <p id="cen">Senha</p><input id="cen" type="password" name="senha1" value="<?=@$cliente['senha']?>"><br><br>
+    <p id="cen">CPF</p><input id="cen" type="text" name="cpf" value="<?=@$cliente['cpf']?>"><br><br>
+    <p id="cen">Data de Nascimento</p><input id="cen" type="text" name="ddn" value="<?=@$cliente['ddn']?>"><br><br>
     <p id="cen">Tipo</p><input id="cen" type="text" name="tipo" value="<?=@$cliente['tipo']?>"><br><br>
+    <select name=""
     <button type="submit" id="texte">Enviar</button>
+    
 </form>
